2f15976817629d1c642485e188169a9f54316950
lorenzo <lorenzo@Lenovo-PC>
2019-05-08 20:36:19 -0300

    Aeropuerto Salida/Llegada + Vuelos

---
+++ Trabajo Practico Anual/src/edu/usal/negocio/AeropuertoSalida.java
@@ -0,0 +1,49 @@
+package edu.usal.negocio;
+
+public class AeropuertoSalida extends Vuelos{
+
+	private String identificacionAeropuerto, Ciudad, Provincia, Pais;
+
+	public AeropuertoSalida() {}
+	
+	public AeropuertoSalida(String identificacionAeropuerto, String ciudad, String provincia, String pais) {
+		super();
+		this.identificacionAeropuerto = identificacionAeropuerto;
+		Ciudad = ciudad;
+		Provincia = provincia;
+		Pais = pais;
+	}
+
+	public String getIdentificacionAeropuerto() {
+		return identificacionAeropuerto;
+	}
+
+	public void setIdentificacionAeropuerto(String identificacionAeropuerto) {
+		this.identificacionAeropuerto = identificacionAeropuerto;
+	}
+
+	public String getCiudad() {
+		return Ciudad;
+	}
+
+	public void setCiudad(String ciudad) {
+		Ciudad = ciudad;
+	}
+
+	public String getProvincia() {
+		return Provincia;
+	}
+
+	public void setProvincia(String provincia) {
+		Provincia = provincia;
+	}
+
+	public String getPais() {
+		return Pais;
+	}
+
+	public void setPais(String pais) {
+		Pais = pais;
+	}
+	
+}
--- Trabajo Practico Anual/src/edu/usal/negocio/Vuelos.java
+++ Trabajo Practico Anual/src/edu/usal/negocio/Vuelos.java
@@ -1,5 +1,63 @@
 package edu.usal.negocio;
 
-public class Vuelos {
- //sobenme el pitito
+import java.time.LocalDateTime;
+
+public abstract class Vuelos {
+	
+	public Vuelos () {}
+	private String numeroVuelo, tiempoVuelo;
+	private int cantidadAsientos;
+	private LocalDateTime fechaHoraSalida, fechaHoraLlegada;
+	
+	public Vuelos(String numeroVuelo, String tiempoVuelo, int cantidadAsientos, LocalDateTime fechaHoraSalida,
+			LocalDateTime fechaHoraLlegada) {
+		super();
+		this.numeroVuelo = numeroVuelo;
+		this.tiempoVuelo = tiempoVuelo;
+		this.cantidadAsientos = cantidadAsientos;
+		this.fechaHoraSalida = fechaHoraSalida;
+		this.fechaHoraLlegada = fechaHoraLlegada;
+	}
+
+	public String getNumeroVuelo() {
+		return numeroVuelo;
+	}
+
+	public void setNumeroVuelo(String numeroVuelo) {
+		this.numeroVuelo = numeroVuelo;
+	}
+
+	public String getTiempoVuelo() {
+		return tiempoVuelo;
+	}
+
+	public void setTiempoVuelo(String tiempoVuelo) {
+		this.tiempoVuelo = tiempoVuelo;
+	}
+
+	public int getCantidadAsientos() {
+		return cantidadAsientos;
+	}
+
+	public void setCantidadAsientos(int cantidadAsientos) {
+		this.cantidadAsientos = cantidadAsientos;
+	}
+
+	public LocalDateTime getFechaHoraSalida() {
+		return fechaHoraSalida;
+	}
+
+	public void setFechaHoraSalida(LocalDateTime fechaHoraSalida) {
+		this.fechaHoraSalida = fechaHoraSalida;
+	}
+
+	public LocalDateTime getFechaHoraLlegada() {
+		return fechaHoraLlegada;
+	}
+
+	public void setFechaHoraLlegada(LocalDateTime fechaHoraLlegada) {
+		this.fechaHoraLlegada = fechaHoraLlegada;
+	}
+	
+	
 }
