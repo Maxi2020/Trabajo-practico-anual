b0b3bd756da3b3114d65254078a77384eb21018f
Maxi <Usuario@MIPC>
2019-11-30 21:17:04 -0300

    commit por error

--- Trabajo Practico Anual/src/edu/usal/negocio/dominio/Ventas.java
+++ Trabajo Practico Anual/src/edu/usal/negocio/dominio/Ventas.java
@@ -6,9 +6,7 @@ import java.time.LocalDateTime;
 
 public class Ventas implements Serializable {
 
-	/**
-	 * 
-	 */
+	
 	private static final long serialVersionUID = 1L;
 	private Clientes cliente;
 	private Vuelos vuelo;
